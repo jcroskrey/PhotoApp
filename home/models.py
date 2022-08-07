42ff1e06b27e2f7cd99399cd250e52b09aaefb74
jcrosk <jcrosk@cox.net>
2022-08-06 20:42:13 -0400

    Photo model, can upload photos through admin site

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
+from distutils.command.upload import upload
+from tkinter import image_names
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    image = models.ImageField(upload_to='photos/')
+    downloads = models.IntegerField(default=0)
+    title = models.TextField(max_length=200, default='title')
+
+    def __str__(self) -> str:
+        return self.title
